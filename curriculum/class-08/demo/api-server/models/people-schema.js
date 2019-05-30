dd7a94f482cabb2daf2dfc4200ae8168ff42fa83
Keith Dahlby <keith@newbo.co>
2019-05-30 11:25:35 -0500

    Add Mongoose People schema

--- curriculum/class-08/demo/api-server/models/people-schema.js
+++ curriculum/class-08/demo/api-server/models/people-schema.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const mongoose = require('mongoose');
+
+const peopleSchema = mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+// If Mongoose already has a people, don't register a new one
+const People = mongoose.models.people ||
+  // Otherwise create people model from schema
+  mongoose.model('people', peopleSchema);
+
+module.exports = People;
