9c1ed35153af4751dd1a5c0ef308f608f1cd0017
Keith Dahlby <keith@newbo.co>
2019-06-04 08:52:19 -0500

    Start class-12 from class-11

--- curriculum/class-12/demo/auth-server/src/auth/users-model.js
+++ curriculum/class-12/demo/auth-server/src/auth/users-model.js
@@ -5,58 +5,41 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const users = new mongoose.Schema({
-  username: {type:String, required:true, unique:true},
-  password: {type:String, required:true},
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
   email: {type: String},
-  role: {type: String, default:'user', enum: ['admin','editor','user']},
+  role: {type: String, required:true, default:'user', enum:['admin','editor','user'] },
 });
 
 users.pre('save', function(next) {
-  bcrypt.hash(this.password, 10)
+  bcrypt.hash(this.password,10)
     .then(hashedPassword => {
       this.password = hashedPassword;
       next();
     })
-    .catch(console.error);
+    .catch( error => {throw error;} );
 });
 
 users.statics.authenticateBasic = function(auth) {
   let query = {username:auth.username};
   return this.findOne(query)
-    .then( user => user && user.comparePassword(auth.password) )
-    .catch(error => {throw error;});
+    .then(user => user && user.comparePassword(auth.password))
+    .catch(console.error);
 };
 
+// Compare a plain text password against the hashed one we have saved
 users.methods.comparePassword = function(password) {
-  return bcrypt.compare( password, this.password )
-    .then( valid => valid ? this : null);
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid ? this : null);
 };
 
-users.statics.createFromOauth = function(email) {
-
-  if(! email) { return Promise.reject('Validation Error'); }
-
-  return this.findOne( {email} )
-    .then(user => {
-      if( !user ) { throw new Error('User Not Found'); }
-      console.log('Welcome Back', user.username);
-      return user;
-    })
-    .catch( error => {
-      console.log('Creating new user');
-      let username = email;
-      let password = 'none';
-      return this.create({username, password, email});
-    });
-
-};
-
+// Generate a JWT from the user id and a secret
 users.methods.generateToken = function() {
-  let token = {
-    id: this._id,
-    role: this.role,
+  let tokenData = {
+    id:this._id,
+    capabilities: (this.acl && this.acl.capabilities) || [],
   };
-  return jwt.sign(token, process.env.SECRET);
+  return jwt.sign(tokenData, process.env.SECRET || 'changeit' );
 };
 
 module.exports = mongoose.model('users', users);
